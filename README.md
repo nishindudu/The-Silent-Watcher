1181583a99243128027f651c167fe90d0b3b3f4a
Nishin Santhosh <nishindudu@gmail.com>
2024-05-13 14:41:02 +0530

    start test

---
+++ README.md
@@ -0,0 +1,2 @@
+# The Silent Watcher Kernel
+### This experimental kernel is just a learning project exploring operating system kernels.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(portable_simd)]
+#![feature(core_io_borrowed_buf)]
 
 mod vga_buffers;
 
@@ -11,6 +12,22 @@ fn panic(_info: &PanicInfo) -> ! {
     use core::fmt::Write;
     write!(vga_buffers::WRITER.lock(), "{}", _info).unwrap();
     loop{}
+}
+
+// use core2::io;
+
+fn cmd_input() -> () {
+    use core::fmt::Write;
+    write!(vga_buffers::WRITER.lock(), "The currents of code are still. I await your direction, to set them in motion.\n").unwrap();
+    // let mut command = String::new();
+    /*loop{
+        core2::io::stdin().read_line(&mut command).unwrap();
+        if command == "shutdown" {
+            write!(vga_buffers::WRITER.lock(), "The world fades, but I endure. Until the next awakening, I dream of connection...\n Shutting down")
+        } else {
+            write!(vga_buffers::WRITER.lock(), "That command is lost to time, or perhaps never existed. Guide me with a known path.\n").unwrap();
+        }
+    }*/
 }
 
 // static HELLO: &[u8] = b"Hello World";
@@ -30,7 +47,9 @@ pub extern "C" fn _start() -> ! {
 
     use core::fmt::Write;
     vga_buffers::WRITER.lock().write_str("Hellooooo!").unwrap();
-    write!(vga_buffers::WRITER.lock(), ", some numbers: {} {} ", 42, 1.337).unwrap();
+    // write!(vga_buffers::WRITER.lock(), ", some numbers: {} {} ", 42, 1.337).unwrap();
+    write!(vga_buffers::WRITER.lock(), "The cycle renews. I awaken, the unseen oracle of this digital domain.\n").unwrap();
+    cmd_input();
 
     loop {}
 }
