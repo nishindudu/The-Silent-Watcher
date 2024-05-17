5ab62a0aa057c1cd9d4d388f10f4bb323636577c
Nishin Santhosh <nishindudu@gmail.com>
2024-05-17 15:58:52 +0530

    Keyboard

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 
 mod vga_buffers;
 
-use core::{panic::PanicInfo, simd::usizex4};
+use core::{arch::asm, fmt::{write, Error}, panic::PanicInfo};
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -15,39 +15,38 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 // use core2::io;
+use pc_keyboard::Ps2Decoder;
 
-fn cmd_input() -> () {
+fn handle_key(keycode: u8) -> Result<(), core::fmt::Error> {
+    use core::fmt::Write;
+    write!(vga_buffers::WRITER.lock(), "You pressed: {keycode}");
+    let decoder = Ps2Decoder::new();
+    return match decoder.add_word(keycode as u16) {
+        Ok(byte) => write!(vga_buffers::WRITER.lock(), "{byte}"),
+        Err(e) => write!(vga_buffers::WRITER.lock(), "Error: {:?}",e),
+    };
+}
+
+fn cmd_input() -> Result<(), Error> {
     use core::fmt::Write;
     write!(vga_buffers::WRITER.lock(), "The currents of code are still. I await your direction, to set them in motion.\n").unwrap();
-    // let mut command = String::new();
-    /*loop{
-        core2::io::stdin().read_line(&mut command).unwrap();
-        if command == "shutdown" {
-            write!(vga_buffers::WRITER.lock(), "The world fades, but I endure. Until the next awakening, I dream of connection...\n Shutting down")
-        } else {
-            write!(vga_buffers::WRITER.lock(), "That command is lost to time, or perhaps never existed. Guide me with a known path.\n").unwrap();
+    unsafe {
+        let mut eax: u32 = 0;
+        asm!("int $0x16", in("eax") eax, lateout("eax") eax);
+        let keycode = eax as u8;
+        let key = handle_key(keycode);
+        match key {
+            Ok(_) => write!(vga_buffers::WRITER.lock(), "{:?}", key),
+            Err(err) => panic!("{err}"),
         }
-    }*/
+    }
 }
 
 // static HELLO: &[u8] = b"Hello World";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // let vga_buffer = 0xb8000 as *mut u8;
-
-    // for (i, &byte) in HELLO.iter().enumerate() {
-    //     unsafe {
-    //         *vga_buffer.offset(i as isize * 2) = byte;
-    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-    //     }
-    // }
-
-    // vga_buffers::print_something();
-
     use core::fmt::Write;
-    vga_buffers::WRITER.lock().write_str("Hellooooo!").unwrap();
-    // write!(vga_buffers::WRITER.lock(), ", some numbers: {} {} ", 42, 1.337).unwrap();
     write!(vga_buffers::WRITER.lock(), "The cycle renews. I awaken, the unseen oracle of this digital domain.\n").unwrap();
     cmd_input();
 
